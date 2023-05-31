259d2253f6ae07261b45f1cc4ccf05fc8314d5af
Ignacio <projetosignacio@gmail.com>
2023-05-31 08:23:45 -0300

    Criação dos estilos globais

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import GlobalStyles from './styles/global'
+
 import App from './App';
 
 import { Home } from './pages/Home';
@@ -10,6 +13,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <Home />
   </React.StrictMode>
 );
---
+++ src/styles/global.js
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from'styled-components';
+
+export default createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: gray;
+        color: white;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    body, input, textarea, button{
+        font-size: 16px;
+        outline: none;
+    } 
+
+    a{
+        text-decoration: none;
+    }
+
+    button, a{
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover{
+        filter: brightness(0.9);
+    }
+`;
